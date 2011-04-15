5a7bc5dd874f708f93478bf5825a0ad795b2dd2b
Jon Spalding <jon.spalding@gmail.com>
2011-04-15 10:32:22 +0100

    Baseline dsl working

--- lib/misty/dsl.rb
+++ lib/misty/dsl.rb
@@ -11,6 +11,10 @@ module Misty
     end
 
     class Project
+      def initialize
+        @formations = {}
+      end
+      
       def project(name)
         @name = name
       end
@@ -22,21 +26,53 @@ module Misty
       def key_pair(name)
         @key_pair = name
       end
+      
+      def formation(name, &block)
+        f = Formation.new(name)
+        f.instance_eval(&block)
+        @formations[name] = f
+      end
     end
     
-    # class Formation
-    #   def server_group(&block)
-    #   end
-    # end
-    # 
-    # class ServerGroup
-    #   def size(size)
-    #     
-    #   end
-    # end
+    class Formation
+      def initialize(name)
+        @name = name
+        @server_groups = {}
+      end
+      
+      def server_group(name, &block)
+        server_group = ServerGroup.new(name)
+        server_group.instance_eval(&block)
+        @server_groups[name] = server_group
+      end
+    end
+    
+    class ServerGroup
+      def initialize(name)
+        @name = name
+        @instances = 1
+        @size = "t1.micro"
+      end
+      
+      def size(size)
+        @size = size
+      end
+      
+      def ami(ami)
+        @ami = ami
+      end
+      
+      def security_groups(*groups)
+        @security_groups = groups
+      end
+      
+      def instances(num)
+        @instances = num
+      end
+    end
   end
   
-  class DslError
-    
-  end
+  # class DslError
+  #   
+  # end
 end
--- test/dsl/simple_test.rb
+++ test/dsl/simple_test.rb
@@ -9,6 +9,15 @@ class SimpleTest < Test::Unit::TestCase
     project "badger_wars"
     key_pair "my_key_pair"
     region "eu-west-1"
+    
+    formation :dev do
+      server_group :web do 
+        instances 1
+        size "m1.small"
+        ami "ami-fb9ca98f"
+        security_groups :default
+      end
+    end
     )
     
     defintition = Misty::Definition.new simple
@@ -17,6 +26,15 @@ class SimpleTest < Test::Unit::TestCase
     assert_equal "badger_wars", project.instance_variable_get(:@name)
     assert_equal "my_key_pair", project.instance_variable_get(:@key_pair)
     assert_equal "eu-west-1", project.instance_variable_get(:@region)
+    
+    formation = project.instance_variable_get(:@formations)[:dev]
+    assert_equal :dev, formation.instance_variable_get(:@name)
+    
+    server_group = formation.instance_variable_get(:@server_groups)[:web]
+    assert_equal 1, server_group.instance_variable_get(:@instances)
+    assert_equal "m1.small", server_group.instance_variable_get(:@size)
+    assert_equal "ami-fb9ca98f", server_group.instance_variable_get(:@ami)
+    assert_equal [:default], server_group.instance_variable_get(:@security_groups)
   end
   
 end
