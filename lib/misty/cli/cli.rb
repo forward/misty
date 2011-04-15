3353f1c0feab0b0aed6ba7630d4ba505ebab9b82
Jon Spalding <jon.spalding@gmail.com>
2011-04-15 16:21:06 +0100

    Use Environment variables for AWS credentials

--- lib/misty/cli/cli.rb
+++ lib/misty/cli/cli.rb
@@ -7,7 +7,10 @@ module Misty
   
   module Config
     def config
-      @config ||= YAML.load(File.read(File.join(Dir.pwd, "ec2.yml")))
+      {
+        :aws_access_key_id => ENV["AWS_ACCESS_KEY_ID"],
+        :aws_secret_access_key => ENV["AWS_SECRET_ACCESS_KEY"]
+      }
     end
     
     def load_project
