42ed679dd5617479898a0b112fe4af37d666df2d
Jon Spalding <jon.spalding@gmail.com>
2011-04-15 12:33:24 +0100

    Add support for security groups and image sizes and multiple instances

--- lib/misty/template.rb
+++ lib/misty/template.rb
@@ -1,14 +1,4 @@
 module Misty
-  module InstanceMethods
-    def attribute(attr_name)
-      instance_variable_get(attr_name)
-    end
-  end
-  
-  class Dsl::Project; include InstanceMethods;end
-  class Dsl::Formation; include InstanceMethods;end
-  class Dsl::ServerGroup; include InstanceMethods;end
-  
   class Template
     def initialize(project)
       @project = project
@@ -17,51 +7,47 @@ module Misty
     def generate(formation_name)
       hash = {
         "AWSTemplateFormatVersion" => "2010-09-09",
-        "Description" => "Misty generated template for #{@project.attribute(:@name)}",
-        "Resources" => {
-          "Ec2Instance" => {
-            "Type" => "AWS::EC2::Instance",
-            "Properties" => {
-              "KeyName" => @project.attribute(:@key_pair),
-              "ImageId" => @project.attribute(:@formations)[formation_name].attribute(:@server_groups)[:web].attribute(:@ami)
-            }
-          }
-        },
-        "Outputs" => {
-          "InstanceId" => {
-             "Description" => "The InstanceId of the newly created EC2 instance",
-             "Value" => { "Ref" => "Ec2Instance" }
-           }
-        }
+        "Description" => "Misty generated template for #{@project._name}",
+        "Resources" => resources(@project._formations[formation_name]),
+        # "Outputs" => {
+        #   "InstanceId" => {
+        #      "Description" => "The InstanceId of the newly created EC2 instance",
+        #      "Value" => { "Ref" => "Ec2Instance" }
+        #    }
+        # }
       }
     end
-  end
-end
-
-%q(
-{
-    "Description" : "Create an EC2 instance running the Amazon Linux 32 bit AMI.”
-    "Parameters" : {
-        "KeyPair" : {
-            "Description" : "The EC2 Key Pair to allow SSH access to the instance",       
-            "Type" : "String"
-        }
-    },
-    "Resources" : {
-        "Ec2Instance" : {
-            "Type" : "AWS::EC2::Instance",
-            "Properties" : {
-                "KeyName" : { "Ref" : "KeyPair" },
-                "ImageId" : "ami-75g0061f"
+    
+    private
+    def resources(formation)
+      res = {}
+      
+      formation._server_groups.each do |name, server_group|
+        security_groups = server_group._security_groups.is_a?(Array) ? server_group._security_groups.map{|sg| sg.to_s} : [server_group._security_groups.to_s]
+        
+        server_group._instances.times do |i|
+          res["#{name}ServerGroupInstance#{i}"] = {
+            "Type" => "AWS::EC2::Instance",
+            "Properties" => {
+              "KeyName" => @project._key_pair,
+              "ImageId" => server_group._ami,
+              "InstanceType" => server_group._size,
+              "SecurityGroups" => security_groups
             }
-        }
-    },
-    "Outputs" : {
-        "InstanceId" : {
-             "Description" : "The InstanceId of the newly created EC2 instance",
-             "Value" : { "Ref" : "Ec2Instance" }
-         }
-    },
-    "AWSTemplateFormatVersion" : "2010-09-09"
-}
-)
+          }
+        end
+      end
+      res
+    end
+  end
+  
+  module AttributeAccessor
+    def method_missing(m, *args)
+      instance_variable_get("@#{m.to_s.sub(/_/, '')}".to_sym) if (m.to_s =~ /^_/)
+    end
+  end
+  
+  class Dsl::Project; include AttributeAccessor; end
+  class Dsl::Formation; include AttributeAccessor; end
+  class Dsl::ServerGroup; include AttributeAccessor; end
+end
--- test/unit/template_test.rb
+++ test/unit/template_test.rb
@@ -6,7 +6,7 @@ require File.dirname(__FILE__) + "/../../lib/misty"
 
 class TemplateTest < Test::Unit::TestCase
 
-  def test_template
+  def test_single_server_group
     simple = %q(
     project "badger_wars"
     key_pair "ih-key"
@@ -14,7 +14,7 @@ class TemplateTest < Test::Unit::TestCase
     
     formation :dev do
       server_group :web do 
-        instances 1
+        instances 2
         size "m1.small"
         ami "ami-ccf405a5"
         security_groups :default
@@ -33,8 +33,14 @@ class TemplateTest < Test::Unit::TestCase
     end
     
     assert_equal "Misty generated template for badger_wars", hash["Description"]
-    assert_equal "ih-key", hash["Resources"]["Ec2Instance"]["Properties"]["KeyName"]
-    assert_equal "ami-ccf405a5", hash["Resources"]["Ec2Instance"]["Properties"]["ImageId"]
+    assert_equal "ih-key", hash["Resources"]["webServerGroupInstance0"]["Properties"]["KeyName"]
+    assert_equal "ih-key", hash["Resources"]["webServerGroupInstance1"]["Properties"]["KeyName"]
+    assert_equal "ami-ccf405a5", hash["Resources"]["webServerGroupInstance0"]["Properties"]["ImageId"]
+    assert_equal "ami-ccf405a5", hash["Resources"]["webServerGroupInstance1"]["Properties"]["ImageId"]
+    assert_equal "m1.small", hash["Resources"]["webServerGroupInstance0"]["Properties"]["InstanceType"]
+    assert_equal "m1.small", hash["Resources"]["webServerGroupInstance1"]["Properties"]["InstanceType"]
+    assert_equal ["default"], hash["Resources"]["webServerGroupInstance0"]["Properties"]["SecurityGroups"]
+    assert_equal ["default"], hash["Resources"]["webServerGroupInstance1"]["Properties"]["SecurityGroups"]
     
   end
 
